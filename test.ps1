34d28bb20a27f7366642e6187a4c0ccbbddff4bc
saisrinivas58 <36913542+saisrinivas58@users.noreply.github.com>
2020-08-21 20:30:36 +0530

    Create test.ps1

---
+++ test.ps1
@@ -0,0 +1,36 @@
+param(
+    [parameter(Mandatory=$true)]
+	[String] $resourceGroupName,
+    [parameter(Mandatory=$true)]
+	[String] $location,
+	[parameter(Mandatory=$true)]
+	[String] $keyVaultName,
+	[parameter(Mandatory=$true)]
+	[String] $sku,
+	[parameter(Mandatory=$true)] 	
+)
+
+Function keyvaultCreate()
+{
+$keyVault=Get-AzureRMKeyVault -VaultName $keyVaultName -ErrorVariable notPresent -ErrorAction SilentlyContinue
+$userObjectId = (Get-AzureRmADUser -SearchString $user).Id 
+if (!$keyVault)
+{
+#creating Keyvault in azure
+New-AzureRmKeyVault -VaultName $keyVaultName -ResourceGroupName $resourceGroupName -Location $location -SKU $SKU
+# assigning Access policies to user
+
+ 
+}
+else 
+{
+Write-Output " keyVault already presented"
+
+}
+}
+
+
+
+
+
+keyvaultCreate
